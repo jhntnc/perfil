3cfe73181b99aba2c66ad312398aa409f0027d15
jhntnc <JHONATANCALDERON2104@GMAIL.COM>
2022-12-12 01:01:52 +0000

    borrado los null

--- src/js/app.js
+++ src/js/app.js
@@ -33,9 +33,13 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name} ${variables.lastname}</h1>
-          <h2>${variables.role}</h2>
-          <h3>${variables.city}, ${variables.country}</h3>
+          <h1>${variables.name == null ? "" : variables.name} ${
+    variables.lastname == null ? "" : variables.lastname
+  }</h1>
+          <h2>${variables.role == null ? "" : variables.role}</h2>
+          <h3>${variables.city == null ? "" : variables.city}, ${
+    variables.country == null ? "" : variables.country
+  }</h3>
           <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
